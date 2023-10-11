6a07cf5792f5b5ecdea0e5a87041261dbd80cc05
HammiAhlan <hammiahlan@gmail.com>
2023-10-11 16:05:32 +0700

    ya begitulah

---
+++ Praktikum02/Praktikum/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_1_714220062
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Angka 1");
+            int angka1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Angka 2");
+            int angka2 =Convert.ToInt32(Console.ReadLine());
+
+
+            int penjumlahan;
+            int pengurangan;
+            int perkalian;
+            float pembagian;
+
+            penjumlahan = angka1 + angka2;
+            pengurangan = angka1 - angka2;
+            perkalian = angka1 * angka2;
+            pembagian = (float)angka1 / angka2;
+
+            Console.WriteLine($"{angka1} + {angka2} = {penjumlahan}");
+            Console.WriteLine($"{angka1} - {angka2} = {pengurangan}");
+            Console.WriteLine($"{angka1} * {angka2} = {perkalian}");
+            Console.WriteLine($"{angka1} / {angka2} = {pembagian}");
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ Praktikum03/Praktikum/P3_2_714220062/P3_2_714220062/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P3_2_714220062
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.Write("MASUKKAN BUDAK AYAM\n------------------------------");
+            Console.Write("\nMasukan Imputan BUDAK Ayam: ");
+
+            int ayam = Convert.ToInt32(Console.ReadLine());
+            if (ayam >= 1 && ayam <= 10)
+            {
+                for (int i = ayam; i >= 1; i--)
+                {
+                    Console.WriteLine($"Anak ayam turunlah {i}, mati satu tinggallah {i - 1}");
+                }
+                Console.WriteLine("Mati satu tinggallah i");
+            }
+            else
+            {
+                Console.WriteLine("Nilai tidak boleh dibawah 1 atau diatas 10");
+            }
+        }
+    }
+}
