09c9242967116e2d071b894677d4a7e89ff5daec
HammiAhlan <hammiahlan@gmail.com>
2023-10-19 21:13:22 +0700

    Pemograman

---
+++ Praktikum04/Praktikum/P4_1_714220062/P4_1_714220062/Book_714220062.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P4_1_714220062
+{
+    public class Book_714220062 : Product_714220062
+    {
+        protected string pageCount;
+
+        public Book_714220062(string type, string title, string pageCount) : base(type , title)
+        {
+            this.pageCount = pageCount;
+        }
+        public string PageCount
+        { get 
+            { 
+                return pageCount; 
+            } 
+            set { pageCount = value; }
+        }
+        
+
+    }
+}
