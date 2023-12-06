1911acddc60fcd53af428f8c256e5d6e4c80f3da
HammiAhlan <hammiahlan@gmail.com>
2023-12-06 15:03:34 +0700

    PBO Xampp

---
+++ Praktikum10/Praktikum/P10_7142200062/P10_7142200062/MySqlCommand.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace P10_7142200062
+{
+    internal class MySqlCommand
+    {
+        private string query;
+        private MySqlConnection kon;
+
+        public MySqlCommand(string query, MySqlConnection kon)
+        {
+            this.query = query;
+            this.kon = kon;
+        }
+
+        internal void ExecuteNonQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
